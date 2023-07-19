525888013f37066695beed62ba1adc823025f8a2
EngyKhaled209 <gogokhaled209@example.com>
2023-07-19 15:48:57 -0700

    qwertyu

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * print_to_98 - Entry point
@@ -10,34 +9,34 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 		printf("%d", n);
-		_putchar('\n');
+		putchar('\n');
 	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			_putchar(' ');
-			if (n == 98)
-			{
-				continue;
-			}
-			_putchar(',');
-		}
-		_putchar('\n');
-	}
-	else if (n > 98)
-	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			_putchar(' ');
+			putchar(' ');
 			if (n == 98)
 			{
 				continue;
 			}
 			putchar(',');
 		}
-		_putchar('\n');
+		putchar('\n');
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			putchar(' ');
+			if (n == 98)
+			{
+				continue;
+			}
+			putchar(',');
+		}
+		putchar('\n');
 	}
 }
