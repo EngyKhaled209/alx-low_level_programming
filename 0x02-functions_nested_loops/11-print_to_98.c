3044b39b8051148b20070c9413b195204b04b29e
EngyKhaled209 <gogokhaled209@example.com>
2023-07-19 15:37:45 -0700

    loloy

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Entry point
  * @n: an integar number
@@ -9,14 +10,14 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 		printf("%d", n)
-		_putchar ('\n');
+		_putchar('\n');
 	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			_putchar (' ');
+			_putchar(' ');
 			if (n == 98)
 			{
 				continue;
