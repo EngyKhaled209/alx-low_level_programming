c7c3053da59738f57cbc1d11a1a16421922391a8
EngyKhaled209 <gogokhaled209@example.com>
2023-07-19 08:42:10 -0700

    koko

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			putchar(i)
+		}
+		putchar('\n');
+	}
+}
