be7c147e3e3466e27b62c9246fc41cea7f53aa20
EngyKhaled209 <gogokhaled209@example.com>
2023-07-20 16:22:54 -0700

    lolouiy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: an integar number
+ * @b: a second integar number
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
