6197131449bf15c82434a12418abf652317ce504
EngyKhaled209 <gogokhaled209@example.com>
2023-07-21 12:46:02 -0700

    fghj

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,12 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\\n');
+		_putchar('\n');
+
 	}
 	else
 	{
-		for (j = 0; j <= n; j++)
+		for (j = 0; j < n; j++)
 		{
 			for (space = 1; space <= j; space++)
 			{
