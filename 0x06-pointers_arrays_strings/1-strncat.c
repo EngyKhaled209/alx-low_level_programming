efe4dd5ef6eabe6e304d8e0822fef18f6b5e5ad7
EngyKhaled209 <gogokhaled209@example.com>
2023-07-27 14:59:49 -0700

    popo

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * _strncat - Entry point
+ * @src: character
+ * @dest: second character
+ * @n: integar number
+ * Return: Always 0 (success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	int c;
+
+	while (dest[c])
+	{
+		c++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[c + i] = src[i];
+	}
+	dest[c + i] != '\0';
+	return (dest);
+}
