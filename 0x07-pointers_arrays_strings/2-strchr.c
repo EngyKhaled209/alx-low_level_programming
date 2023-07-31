61a45f75f7621b801a71e391be567c09bd49ad78
EngyKhaled209 <gogokhaled209@example.com>
2023-07-31 16:47:26 -0700

    jjj

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: first character
+ * @c: second character
+ * Return: Always 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+		{
+			return (&s[i]);
+		}
+	}
+	return (0);
+}
