c40977c7c3a3d9998121f205b754435507409903
EngyKhaled209 <gogokhaled209@example.com>
2023-08-03 19:31:33 -0700

    ooo

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include "main.h"
+int actual(int n, int val);
 /**
  * _sqrt_recursion - Entry point
  * @n: first int
- * @val: second int
  * Return: Always 0 (success)
  */
-int _sqrt_recursion(int n, int val)
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (actual(n, 0));
+}
+/**
+ * actual - second function
+ * @n: an integar
+ * @val: second integar
+ * Return: the sqrt
+ */
+int actual(int n, int val)
 {
 	if (val * val == n)
 	{
 		return (val);
 	}
-	else if (val * val < n)
-	{
-		return (_sqrt_recursion(n, val + 1));
-	}
-	else
+	if (val * val > n)
 	{
 		return (-1);
 	}
+	return (actual(n, val + 1))
 }
