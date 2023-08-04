4bc03466f2dbe3664a4d60e6e6c1f295deebfd5a
EngyKhaled209 <gogokhaled209@example.com>
2023-08-03 17:27:16 -0700

    qwqwww

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - Entry point
+ * @s: is a character
+ * Return: Always 0 (success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*c);
+	}
+}
