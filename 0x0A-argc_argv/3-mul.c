b5a23588fd3b99c2fdabc3a3c3a8abd72e530b1f
EngyKhaled209 <gogokhaled209@example.com>
2023-08-08 10:05:37 -0700

    rere

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -53,17 +53,19 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int mul;
+	int mul, num1, num2;
 
-	if (argc == 3)
-	{
-		mul = _atoi(argv[1]) * _atoi(argv[2]);
-		ptintf("%d\n", mul);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num1 = _atoi(argv[1]);
+
+	num2 = _atoi(argv[2]);
+
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
 	return (0);
 }
