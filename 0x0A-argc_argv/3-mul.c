a39b98eeef34527caa916ae2d6949bb7c2b8c9ce
EngyKhaled209 <gogokhaled209@example.com>
2023-08-08 09:59:38 -0700

    rrrr

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+/**
+ * _atoi - convert string to integar
+ * @s: sting to convert
+ * Return: what is converted
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+
+	d = 0;
+
+	n = 0;
+
+	len = 0;
+
+	f = 0;
+	digit = 0;
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
 /**
  * main - Entry point
  * @argc: an integar num
@@ -14,7 +57,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
+		mul = _atoi(argv[1]) * _atoi(argv[2]);
 		ptintf("%d\n", mul);
 	}
 	else
