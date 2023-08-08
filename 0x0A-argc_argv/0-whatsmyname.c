15c49a281e31c77b3a207c07e6c0a0ae09630a2d
EngyKhaled209 <gogokhaled209@example.com>
2023-08-07 17:47:54 -0700

    nnnn

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: an integar
+ * @argv: a character
+ * Return: Always 0 (success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
