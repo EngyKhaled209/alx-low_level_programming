8fa11fea3f5e43431a6deda0dd4a18952e7e20dd
EngyKhaled209 <gogokhaled209@example.com>
2023-08-07 18:27:03 -0700

    qqqq

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: an integar num
+ * @argv: a string
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		ptintf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
